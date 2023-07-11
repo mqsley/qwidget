b19f5896a119ddf65188b8727d06b44f3b085b03
Matt Quinn <mquinn2489@gmail.com>
2023-07-11 13:34:36 -0600

    csv download

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,22 @@
 class MessagesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:show]
-  before_action :set_widget, only: [:show]
+  before_action :authenticate_user!, only: [:index]
+  skip_before_action :verify_authenticity_token, only: [:show, :create]
+  before_action :set_widget
+
+
+
+  def index
+    @messages = current_user.messages.order(created_at: :desc)
+      respond_to do |format|
+        format.html { @messages }
+        format.csv { send_data(Message.to_csv(@messages), filename: "messages.csv") }
+      end
+  end
 
   def create
     @widget.messages.create!(message_params)
     head :ok
   end
-
-  def index
-    @messages = current_user.messages.order(created_at: :desc)
-  end
-
 
   private
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,14 @@ class Message < ApplicationRecord
   belongs_to :widget
   validates_presence_of :name, :email, :content
 
+  def self.to_csv(messages)
+    CSV.generate do |csv|
+      csv << ['created_at', 'name', 'email', 'content']
+
+      messages.each do |message|
+        csv << [message.created_at, message.name, message.email, message.content]
+      end
+    end
+  end
+
 end
